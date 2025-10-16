3b3b0659d2ee1fd01331bcdbb2cee6aacc4ca165
FaisalAhmedNour <faisalahmed61919112@gmail.com>
2025-10-16 21:27:49 +0600

    remove

--- Frontend/src/Components/CustomTable/Drowers/RightDrower.jsx
+++
@@ -1,8 +0,0 @@
-const RightDrawer = () => {
-    return (
-        <div className="border rounded-sm bg-white h-full">
-            <p className="text-center">right drawer</p>
-        </div>
-    )
-}
-export default RightDrawer;
--- Frontend/src/Functions/ConvertToDate.js
+++
@@ -1,8 +0,0 @@
-const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-const convertDateFormate = (date) => {
-    const formatDate = new Date(date);
-    return `${formatDate.getDate() > 9 ? formatDate.getDate() : `0${formatDate.getDate()}`}-${month[formatDate.getMonth()]}-${formatDate.getFullYear()}`;
-} 
-
-export default convertDateFormate;
